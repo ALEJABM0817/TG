ffe436a046b839321eaadef45e0bae7ecf6131fd
Alejandra Bolaños <118947573+ALEJABM0817@users.noreply.github.com>
2024-05-23 20:28:16 -0500

    enviar datos para iniciar sesion

--- src/api/usuarios.api.js
+++ src/api/usuarios.api.js
@@ -2,3 +2,6 @@ import axios from "axios";
 
 export const createUsuario = async (usuario) =>
     await axios.post("http://localhost:4000/user", usuario);
+
+export const getUsuario = async (usuario) =>
+    await axios.get(`http://localhost:4000/users/${usuario.cedula}/${usuario.password}`, usuario);
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
-import Facial from "../../assets/images/icons/id-facial.png"
+import Facial from "../../assets/images/icons/id-facial.png";
 import Candado from "../../assets/images/icons/candado.png";
 import { Link, useNavigate } from "react-router-dom";
+import { Form, Formik } from "formik";
+import { getUsuario } from "../../api/usuarios.api";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
@@ -14,52 +16,85 @@ export const LoginPage = () => {
   return (
     <div className="form-register-container">
       <section className="form-Register">
-          <div className="container-logo">
-            <img
-              className="logo"
-              src="src\assets\images\icons\logoApp.png"
-              alt=""
-            />
-          </div>
-          <div className="container mt-5">
-            <h4>Iniciar sesión</h4>
-            <hr />
-          </div>
+        <Formik
+          initialValues={{
+            cedula: "",
+            password: "",
+          }}
 
-          <div className="container-input">
-            <img src={Facial} />
-            <input
-              className="controls"
-              type="text"
-              name="C.C"
-              id="id"
-              placeholder="Identificación"
-            />
-          </div>
+          onSubmit={ async (values) => {
+            const isEmpty = Object.values(values).some(x => (x === ''));
+            if (isEmpty) {
+                console.log("Los valores son requeridos")
+            }
 
-          <div className="container-input">
-            <img src={Candado} />
-            <input
-              className="controls"
-              type="password"
-              name="Contraseña"
-              id="contraseña"
-              placeholder="Contraseña"
-            />
-          </div>
+            try {
+                const response = await getUsuario(values)
+                console.log(response)
+                
+            } catch (error) {
+                console.log(error.response)
+            }
+          }}
+        >
+          {({handleChange, handleSubmit}) => (
+            <Form
+                onSubmit={handleSubmit}
+            >
+              <div className="container-logo">
+                <img
+                  className="logo"
+                  src="src\assets\images\icons\logoApp.png"
+                  alt=""
+                />
+              </div>
+              <div className="container mt-5">
+                <h4>Iniciar sesión</h4>
+                <hr />
+              </div>
 
-          <input className="buttons" type="submit" value="Ingresar" />
+              <div className="container-input">
+                <img src={Facial} />
+                <input
+                  className="controls"
+                  type="text"
+                  name="cedula"
+                  id="id"
+                  placeholder="Cedula"
+                  onChange={handleChange}
+                />
+              </div>
 
-          <p className="info-text">¿Aún no eres miembro?</p>
-          <p className="info-text">
-            <Link to="/PersonaNatural">
-              Regístrate aquí si quieres contratar un servicio
-            </Link>
-          </p>
-          <p>
-            <Link to="/Empresa">Regístrate aquí si quieres ofrecer un servicio</Link>
-          </p>
+              <div className="container-input">
+                <img src={Candado} />
+                <input
+                  className="controls"
+                  type="password"
+                  name="password"
+                  id="contraseña"
+                  placeholder="Contraseña"
+                  onChange={handleChange}
+                />
+              </div>
 
+              <button className="buttons" type="submit" value="Ingresar">
+                Ingresar
+              </button>
+
+              <p className="info-text">¿Aún no eres miembro?</p>
+              <p className="info-text">
+                <Link to="/PersonaNatural">
+                  Regístrate aquí si quieres contratar un servicio
+                </Link>
+              </p>
+              <p>
+                <Link to="/Empresa">
+                  Regístrate aquí si quieres ofrecer un servicio
+                </Link>
+              </p>
+            </Form>
+          )}
+        </Formik>
       </section>
     </div>
   );
