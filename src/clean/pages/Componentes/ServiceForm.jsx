efb0af0ce09001aa5079b830c3b0f3f01fa2c9d0
Alejandra Bolaños <ana.alejandra.bolanos@correounivalle.edu.co>
2024-11-20 11:18:01 -0500

    cambios en servicios

--- src/clean/pages/Componentes/ServiceForm.jsx
+++ src/clean/pages/Componentes/ServiceForm.jsx
@@ -395,7 +395,7 @@ export const ServiceForm = ({ servicios, idOfertante }) => {
             ))}
 
 
-            <button onClick={handleAddService}>Solicitar servicio</button>
+            <button onClick={handleAddService}>{selectedServices.length > 0 ? 'Añadir otro servicio' : 'Solicitar servicio'}</button>
             {error && <p className="error-message">{error}</p>}
 
             <div className="total">Precio total: {numeral(calculateTotalPrice()).format('0,0')}</div>
